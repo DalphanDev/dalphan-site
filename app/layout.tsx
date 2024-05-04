21a358d93c4ccbdd47e1e9dd9fdc513c02a4b07a
thomasA0509 <tallred2022@fau.edu>
2024-05-04 13:28:55 -0400

    ch

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,18 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Dalphan",
   description: "Hire the best developer for your next project.",
+  generator: "Next.js",
+  applicationName: "Dalphan",
+  referrer: "origin-when-cross-origin",
+  keywords: ["Dalphan", "Developer", "Projects", "Hire"],
+  authors: [{ name: "Thomas Allred" }],
+  creator: "Thomas Allred",
+  publisher: "Thomas Allred",
+  formatDetection: {
+    telephone: true,
+    email: true,
+    address: true,
+  },
 };
 
 export default function RootLayout({
