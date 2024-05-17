f3692ad2be12fd79c0ceba0356773dbd3ed4d06f
Thomas Allred <tjallred02@gmail.com>
2024-05-17 12:08:18 -0400

    page changes

--- app/projects/akamai/page.tsx
+++ app/projects/akamai/page.tsx
@@ -6,47 +6,42 @@ export default function Contact() {
   return (
     <main className="relative flex min-h-screen flex-col items-center">
       <Navbar />
-      <div className="flex justify-between mt-8 space-x-8 w-full px-8">
-        <Image
-          className="shadow-2xl shadow-[#06c6dd]/50 rounded-md flex-shrink"
-          src="/taskPage.png"
-          alt="logo"
-          width={1226}
-          height={667}
-        />
-        <div className="flex flex-col space-y-8 flex-grow">
-          <h1 className="font-bold text-5xl">Hey, I&apos;m Dalphan</h1>
-          <h2 className="font-medium text-xl">
-            I&apos;m a Full Stack Web Developer specializing in:
-          </h2>
-          <ul className="list-disc list-inside font-normal text-lg">
-            <li>HTML/CSS/JS</li>
-            <li>React</li>
-            <li>Node</li>
-            <li>Go</li>
-            <li>Python</li>
-          </ul>
-          <h2 className="font-medium text-xl">
-            Interact with some of my projects below:
-          </h2>
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <Link href="/projects/dalphanaio">
-              <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                Dalphan AIO
-              </button>
-            </Link>
-            <div className="flex justify-between space-x-4">
-              <Link href="/projects/turbo">
-                <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                  Turbo
-                </button>
-              </Link>
-              <Link href="/projects/akamai">
-                <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                  Akamai Sensor
-                </button>
-              </Link>
-            </div>
+      <div className="flex flex-col justify-between items-center mt-8 space-y-8 w-full px-8">
+        <h2 className="font-medium text-4xl">Akamai Sensor</h2>
+        <div className="flex justify-between items-center space-x-8 flex-grow">
+          <iframe
+            className="flex-grow"
+            src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7152803180223819776?compact=1"
+            height="500"
+            width="1200"
+            frameBorder="0"
+            allowFullScreen
+            title="Embedded post"
+          ></iframe>
+          <div className="flex-shrink">
+            <p className="font-medium text-xl mb-8">
+              Akamai Sensor generates realistic sensors that Akamai cannot
+              differentiate between fake and real. Akamai sells anti-bot
+              solutions to companies to ward off bots and web scrapers. This
+              project demonstrates how one can de- obfuscate and reverse
+              engineer a script.
+            </p>
+            <ul className="list-disc list-inside space-y-8 font-small text-lg">
+              <li>
+                Rebuilt the script in Go after gathering all the functions and
+                steps used in generating a "sensor" for Akamai's anti-bot
+                service.
+              </li>
+              <li>
+                Debugged and documented the code utilizing the Chrome DevTools
+                to keep track of variable values while stepping through the code
+                while running.
+              </li>
+              <li>
+                Researched Akamai and their anti-bot solutions to gather
+                information on techniques Akamai employs to stop web scrapers.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
--- app/projects/turbo/page.tsx
+++ app/projects/turbo/page.tsx
@@ -6,47 +6,41 @@ export default function Contact() {
   return (
     <main className="relative flex min-h-screen flex-col items-center">
       <Navbar />
-      <div className="flex justify-between mt-8 space-x-8 w-full px-8">
-        <Image
-          className="shadow-2xl shadow-[#06c6dd]/50 rounded-md flex-shrink"
-          src="/taskPage.png"
-          alt="logo"
-          width={1226}
-          height={667}
-        />
-        <div className="flex flex-col space-y-8 flex-grow">
-          <h1 className="font-bold text-5xl">Hey, I&apos;m Dalphan</h1>
-          <h2 className="font-medium text-xl">
-            I&apos;m a Full Stack Web Developer specializing in:
-          </h2>
-          <ul className="list-disc list-inside font-normal text-lg">
-            <li>HTML/CSS/JS</li>
-            <li>React</li>
-            <li>Node</li>
-            <li>Go</li>
-            <li>Python</li>
-          </ul>
-          <h2 className="font-medium text-xl">
-            Interact with some of my projects below:
-          </h2>
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <Link href="/projects/dalphanaio">
-              <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                Dalphan AIO
-              </button>
-            </Link>
-            <div className="flex justify-between space-x-4">
-              <Link href="/projects/turbo">
-                <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                  Turbo
-                </button>
-              </Link>
-              <Link href="/projects/akamai">
-                <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                  Akamai Sensor
-                </button>
-              </Link>
-            </div>
+      <div className="flex flex-col justify-between items-center mt-8 space-y-8 w-full px-8">
+        <h2 className="font-medium text-4xl">Turbo</h2>
+        <div className="flex justify-between items-center space-x-8 flex-grow">
+          <iframe
+            className="flex-grow"
+            src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7152803180223819776?compact=1"
+            height="500"
+            width="1200"
+            frameBorder="0"
+            allowFullScreen
+            title="Embedded post"
+          ></iframe>
+          <div className="flex-shrink">
+            <p className="font-medium text-xl mb-8">
+              Turbo is a modern request library used for bypassing cybersecurity
+              checks. By mimicking JA3 fingerprints, these requests can closer
+              disguise as other browsers such as Chrome and Firefox.
+            </p>
+            <ul className="list-disc list-inside space-y-8 font-small text-lg">
+              <li>
+                Turbo was able to bypass JA3 protections 100% of the time.
+              </li>
+              <li>
+                Created multiple browser mimics including ones such as Chrome,
+                Firefox, and Safari.
+              </li>
+              <li>
+                Forked off Go's net/http library to make modifications to allow
+                for JA3 editing on the individual requests.
+              </li>
+              <li>
+                Researched salesforce's JA3 security and different browser's JA3
+                fingerprints to document how to appropriately recreate them.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
