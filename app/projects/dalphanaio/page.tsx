d74eafd7eb80ac45cafeb266d32ed52a83797fb8
Thomas Allred <tjallred02@gmail.com>
2024-05-17 12:00:05 -0400

    added dalphan aio page

--- app/projects/dalphanaio/page.tsx
+++ app/projects/dalphanaio/page.tsx
@@ -6,47 +6,45 @@ export default function Contact() {
   return (
     <main className="relative flex min-h-screen flex-col items-center">
       <Navbar />
-      <div className="flex justify-between mt-8 space-x-8 w-full px-8">
-        <Image
-          className="shadow-2xl shadow-[#06c6dd]/50 rounded-md flex-shrink"
-          src="/taskPage.png"
-          alt="logo"
-          width={1226}
-          height={667}
-        />
-        <div className="flex flex-col space-y-8 flex-grow">
-          <h1 className="font-bold text-5xl">Hey, I&apos;m Dalphan</h1>
-          <h2 className="font-medium text-xl">
-            I&apos;m a Full Stack Web Developer specializing in:
-          </h2>
-          <ul className="list-disc list-inside font-normal text-lg">
-            <li>HTML/CSS/JS</li>
-            <li>React</li>
-            <li>Node</li>
-            <li>Go</li>
-            <li>Python</li>
-          </ul>
-          <h2 className="font-medium text-xl">
-            Interact with some of my projects below:
-          </h2>
-          <div className="flex flex-col justify-center items-center space-y-4">
-            <Link href="/projects/dalphanaio">
-              <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                Dalphan AIO
-              </button>
-            </Link>
-            <div className="flex justify-between space-x-4">
-              <Link href="/projects/turbo">
-                <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                  Turbo
-                </button>
-              </Link>
-              <Link href="/projects/akamai">
-                <button className="bg-[#0146a6] rounded-md w-40 py-4 shadow-lg shadow-[#0146a6]/50">
-                  Akamai Sensor
-                </button>
-              </Link>
-            </div>
+      <div className="flex flex-col justify-between items-center mt-8 space-y-8 w-full px-8">
+        <h2 className="font-medium text-4xl">Dalphan AIO</h2>
+        <div className="flex justify-between items-center space-x-8 flex-grow">
+          <iframe
+            className="flex-grow"
+            src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7152803180223819776?compact=1"
+            height="500"
+            width="1200"
+            frameBorder="0"
+            allowFullScreen
+            title="Embedded post"
+          ></iframe>
+          <div className="flex-shrink">
+            <p className="font-medium text-xl mb-8">
+              Dalphan AIO is a bot software designed to streamline and optimize
+              online purchases by automating the buying process across multiple
+              platforms. Leveraging multiple programming languages, DalphanAIO
+              was created to meet modern industry standards.
+            </p>
+            <ul className="list-disc list-inside space-y-8 font-small text-lg">
+              <li>
+                DalphanAIO was able to reduce checkout times by up to 1,000% on
+                some sites.
+              </li>
+              <li>
+                Designed and implemented intuitive user interfaces for the bot,
+                utilizing Electron, React, Redux and Figma for frontend design.
+              </li>
+              <li>
+                Architected and developed the core functionalities of
+                DalphanAIO, writing robust and efficient code primarily in
+                Typescript and Golang.
+              </li>
+              <li>
+                Developed and integrated various APIs to enable seamless
+                interactions between DalphanAIO and online retail platforms,
+                ensuring compatibility and functionality.
+              </li>
+            </ul>
           </div>
         </div>
       </div>
